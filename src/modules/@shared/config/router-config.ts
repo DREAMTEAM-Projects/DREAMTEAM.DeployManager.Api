9c25e31171699100db66255e07ed1254677f1749
Paulo Henrique Oliveira dos Santos <paulo.hosantos@einstein.br>
2023-06-29 17:16:37 -0300

    choreL config team routes

--- src/modules/@shared/config/router-config.ts
+++ src/modules/@shared/config/router-config.ts
@@ -1,6 +1,7 @@
 import { Express, Router } from 'express'
 import templateRoutes from '../../template/infrastructure/api/template.routes'
 import deployRoutes from '../../deploy/infrastructure/api/deploy.routes'
+import teamRoutes from '../../team/infrastructure/api/team.routes'
 import documentRoutes from '../../documents/infrastructure/api/document.routes'
 
 export default (app: Express): void => {
@@ -8,5 +9,6 @@ export default (app: Express): void => {
   templateRoutes(router)
   deployRoutes(router)
   documentRoutes(router)
+  teamRoutes(router)
   app.use(router)
 }
