b86870339557430a4629c037e779084051a67185
Charles Passos <act222@einstein.br>
2023-06-29 13:24:06 -0300

    azure connection implementation

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import { createServer } from "http"
 import express, { Router } from 'express'
 import { makeCreateDeployController } from "./modules/deploy/factory/create-deploy-controller.factory";
 
+/* Connect Azure */
+import * as azdev from 'azure-devops-node-api'
+import * as ba from 'azure-devops-node-api/BuildApi'
+import * as bi from 'azure-devops-node-api/interfaces/BuildInterfaces'
+
 const routes = Router();
 
 routes.post('/deploy', makeCreateDeployController().handle);
@@ -15,3 +20,21 @@ const server = createServer(app)
 server.listen(process.env.PORT || 3000, async () => {
   console.log('Server is running')
 })
+
+/* Azure Connection Implementation */
+
+const orgUrl: string = process.env.AZURE_URL_ORGANIZATION!
+const token = process.env.AZURE_PERSONAL_ACCESS_TOKEN!
+
+const authHandler = azdev.getPersonalAccessTokenHandler(token)
+const connection = new azdev.WebApi(orgUrl, authHandler)
+
+async function run() {
+  let build: ba.IBuildApi = await connection.getBuildApi()
+  let project: string = process.env.AZURE_PROJECT!
+  let defs: bi.DefinitionReference[] = await build.getDefinitions(project)
+
+  defs.forEach((defRef: bi.DefinitionReference) => {
+    console.log(`${defRef.name} (${defRef.id})`)
+  })
+}
